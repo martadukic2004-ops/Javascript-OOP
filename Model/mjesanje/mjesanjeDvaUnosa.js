93a855fa5ea5487c99a976a6a21905c90447252c
moritzanamaria <moritzanamaria@gmail.com>
2026-09-03 16:13:27 +0200

    ažuriranje zvaoAdut i logika štiglje i zvanja

--- Model/mjesanje/mjesanjeDvaUnosa.js
+++ Model/mjesanje/mjesanjeDvaUnosa.js
@@ -1,5 +1,5 @@
- import { Mjesanje } from "./mjesanje.js"
- import { Rezultat } from '../rezultat.js'
+import { Mjesanje } from "./mjesanje.js"
+import { Rezultat } from '../rezultat.js'
 
 export class MjesanjeDvaUnosa extends Mjesanje {
     #bodovaPrviUnos
@@ -7,8 +7,8 @@ export class MjesanjeDvaUnosa extends Mjesanje {
     #zvanjePrviUnos
     #zvanjeDrugiUnos
 
-    constructor(id, stiglja, belot, datumUnosa, bodovaPrviUnos, bodovaDrugiUnos, zvanjePrviUnos, zvanjeDrugiUnos) {
-        super(id, stiglja, belot, datumUnosa)
+    constructor(id, stiglja, belot, datumUnosa, zvaoAdut, bodovaPrviUnos, bodovaDrugiUnos, zvanjePrviUnos, zvanjeDrugiUnos) {
+        super(id, stiglja, belot, datumUnosa, zvaoAdut)
         this.#bodovaPrviUnos = bodovaPrviUnos
         this.#bodovaDrugiUnos = bodovaDrugiUnos
         this.#zvanjePrviUnos = zvanjePrviUnos
@@ -44,9 +44,44 @@ export class MjesanjeDvaUnosa extends Mjesanje {
     }
 
     getRezultat() {
-        return new Rezultat( 
-        this.getBodovaPrviUnos() + this.getZvanjePrviUnos(),
-        this.getBodovaDrugiUnos() + this.getZvanjeDrugiUnos(),
-    )
+        const b1 = this.getBodovaPrviUnos()
+        const b2 = this.getBodovaDrugiUnos()
+        const z1 = this.getZvanjePrviUnos()
+        const z2 = this.getZvanjeDrugiUnos()
+        const zvao = this.getZvaoAdut()
+        const isStiglja = this.getStiglja()
+
+        const ukupnoStihovi = b1 + b2
+        const ukupnoZvanja = z1 + z2
+        const ukupno = ukupnoStihovi + ukupnoZvanja
+
+        if (isStiglja || b1 === 0 || b2 === 0) {
+            if (b1 > 0 && b2 === 0) {
+                return new Rezultat(ukupnoStihovi + 90 + ukupnoZvanja, 0)
+            }
+            if (b2 > 0 && b1 === 0) {
+                return new Rezultat(0, ukupnoStihovi + 90 + ukupnoZvanja)
+            }
+        }
+        if (zvao === 1) {
+            const jeZvaoTotal = b1 + z1
+            const nijeZvaoTotal = b2 + z2
+
+            if (jeZvaoTotal > nijeZvaoTotal) {
+                return new Rezultat(jeZvaoTotal, nijeZvaoTotal)
+            } else {
+                return new Rezultat(0, ukupno)
+            }
+        } else {
+            const jeZvaoTotal = b2 + z2
+            const nijeZvaoTotal = b1 + z1
+
+            if (jeZvaoTotal > nijeZvaoTotal) {
+                return new Rezultat(nijeZvaoTotal, jeZvaoTotal)
+            } else {
+                return new Rezultat(ukupno, 0)
+            }
+        }
     }
+
 }
